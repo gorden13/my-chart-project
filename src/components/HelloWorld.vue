d93028c1283c5ba61952d7ba6293a7d5ee45d1af
denis.goryncev <denis.goryncev@roscomputing.com>
2021-03-29 17:18:15 +0300

    add1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="hello" ref="chartdiv">
     </div>
-    <div>{{ point }}</div>
+    <div><b>{{ point }}</b></div>
   </div>
 </template>
 
@@ -19,8 +19,9 @@ import result from '/data.json'
 export default {
   name: 'HelloWorld',
   data: () => ({
-    points: result?.months || [],
-    point: null
+    points: result?.minutes || [],
+    point: null,
+    clickedBullets: []
   }),
   mounted() {
     this.newRender();
@@ -93,22 +94,45 @@ export default {
 
       // Set input format for the dates
       // chart.dateFormatter.inputDateFormat = "yyyy-MM-dd";
+      let cursorPosition = {
+        x: null,
+        y: null
+      };
 
-      chart.events.on('swipeleft', (ev) => {
-        console.log('swipe left');
+      chart.events.on('up', (ev) => {
+        // console.log('swipe left');
+        
+        console.log(ev);
+        return false
       })
 
-      chart.events.on('swiperight', (ev) => {
-        console.log('swipe right');
-      })
+      // chart.events.on('swipeleft', (ev) => {
+      //   // console.log('swipe left');
+      //   let xAxis = ev.target.xAxes.getIndex(0);
+      //   let yAxis = ev.target.yAxes.getIndex(0);
+      //   cursorPosition.x = xAxis.positionToDate(xAxis.toAxisPosition(ev.target.cursor.xPosition));
+      //   cursorPosition.y = yAxis.positionToValue(yAxis.toAxisPosition(ev.target.cursor.yPosition));
+
+      //   console.log(cursorPosition);
+      // })
+
+      // chart.events.on('swiperight', (ev) => {
+      //   let xAxis = ev.target.xAxes.getIndex(0);
+      //   let yAxis = ev.target.yAxes.getIndex(0);
+      //   cursorPosition.x = xAxis.positionToDate(xAxis.toAxisPosition(ev.target.cursor.xPosition));
+      //   cursorPosition.y = yAxis.positionToValue(yAxis.toAxisPosition(ev.target.cursor.yPosition));
+
+      //   console.log(cursorPosition);
+      // })
 
       // Create axes
       var dateAxis = chart.xAxes.push(new am4charts.DateAxis());
       var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
 
       valueAxis.tooltip.disabled = true;
-      valueAxis.maximum = 86;
-      valueAxis.strictMinMax = true;
+      valueAxis.extraMax = 0.00001
+      valueAxis.extraMin = 0.01
+      valueAxis.start = 0
 
       var gridIntervals = [
         // { timeUnit: "millisecond", count: 1 },
@@ -122,7 +146,7 @@ export default {
         // { timeUnit: "second", count: 10 },
         // { timeUnit: "second", count: 30 },
         // { timeUnit: "minute", count: 1 },
-        // { timeUnit: "minute", count: 5 },
+        { timeUnit: "minute", count: 5 },
         // { timeUnit: "minute", count: 10 },
         // { timeUnit: "minute", count: 30 },
         // { timeUnit: "hour", count: 1 },
@@ -130,43 +154,54 @@ export default {
         // { timeUnit: "hour", count: 3 },
         // { timeUnit: "hour", count: 6 },
         // { timeUnit: "hour", count: 12 },
-        { timeUnit: "day", count: 1 },
-        { timeUnit: "day", count: 2 },
-        { timeUnit: "day", count: 3 },
-        { timeUnit: "day", count: 4 },
-        { timeUnit: "day", count: 5 },
-        { timeUnit: "month", count: 1 },
-        { timeUnit: "month", count: 2 },
-        { timeUnit: "month", count: 3 },
-        { timeUnit: "month", count: 6 },
-        { timeUnit: "month", count: 12 },
-        { timeUnit: "year", count: 1 },
-        { timeUnit: "year", count: 2 },
-        { timeUnit: "year", count: 5 },
-        { timeUnit: "year", count: 10 },
-        { timeUnit: "year", count: 50 },
-        { timeUnit: "year", count: 100 }
+        // { timeUnit: "day", count: 1 },
+        // { timeUnit: "day", count: 2 },
+        // { timeUnit: "day", count: 3 },
+        // { timeUnit: "day", count: 4 },
+        // { timeUnit: "day", count: 5 },
+        // { timeUnit: "month", count: 1 },
+        // { timeUnit: "month", count: 2 },
+        // { timeUnit: "month", count: 3 },
+        // { timeUnit: "month", count: 6 },
+        // { timeUnit: "month", count: 12 },
+        // { timeUnit: "year", count: 1 },
+        // { timeUnit: "year", count: 2 },
+        // { timeUnit: "year", count: 5 },
+        // { timeUnit: "year", count: 10 },
+        // { timeUnit: "year", count: 50 },
+        // { timeUnit: "year", count: 100 }
       ];
       dateAxis.gridIntervals.setAll(gridIntervals);
       dateAxis.baseInterval = {
-        timeUnit: "month",
-        count: 1
+        timeUnit: "minute",
+        count: 5
       };
       // при часоом этот набор
-      // dateAxis.renderer.minGridDistance = 75;
-      // dateAxis.renderer.grid.template.location = 0.5;
-      // dateAxis.startLocation = -0.1;
-      // dateAxis.renderer.labels.template.location = 0.5;
-
-      // при месячном это
-      dateAxis.dateFormats.setKey("month", "MMMM");
-      dateAxis.periodChangeDateFormats.setKey("month", "MMMM");
-      dateAxis.renderer.minGridDistance = 50;
-      dateAxis.startLocation = 0.4;
-      dateAxis.endLocation = 0.6;
+      // dateAxis.dateFormats.setKey("hour", "HH:mm");
+      // dateAxis.periodChangeDateFormats.setKey("hour", "HH:mm");
+      dateAxis.renderer.minGridDistance = 75;
       dateAxis.renderer.grid.template.location = 0;
+      dateAxis.startLocation = -0.1;
+      dateAxis.start = 0.95;
+      // dateAxis.end = 0.3;
       dateAxis.renderer.labels.template.location = 0;
+      
+      // при месячном это
+      // dateAxis.gridIntervals.setAll(gridIntervals);
+      // dateAxis.baseInterval = {
+      //   timeUnit: "month",
+      //   count: 1
+      // };
+      // dateAxis.dateFormats.setKey("month", "MMMM");
+      // dateAxis.periodChangeDateFormats.setKey("month", "MMMM");
+      // dateAxis.renderer.minGridDistance = 50;
+      // dateAxis.startLocation = 0.4;
+      // dateAxis.endLocation = 0.6;
+      // dateAxis.renderer.grid.template.location = 0;
+      // dateAxis.renderer.labels.template.location = 0;
 
+      dateAxis.keepSelection = true;
+      dateAxis.tooltip.disabled = true;
 
       // dateAxis.adapter.add("verticalCenter", function() {
       //   console.log('center');
@@ -194,61 +229,59 @@ export default {
       // axisBreak.breakSize = 0.05;
       // axisBreak.fill = am4core.color("#396478");
 
-      var range = valueAxis.axisRanges.create();
-      range.value = 85;
-      range.grid.stroke = am4core.color("#396478");
-      range.grid.strokeWidth = 2;
-      range.grid.strokeOpacity = 1;
-
-      // Create series
-      var series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.valueY = "value";
-      series.dataFields.dateX = "date";
-      // series.tooltipText = "{value}"
-      series.strokeWidth = 2;
-      series.minBulletDistance = 15;
-      series.dataFields.openValueY = "open";
-      series.dataFields.closeValueY = "close";
-
-      // Make bullets grow on hover
-      var bullet = series.bullets.push(new am4charts.CircleBullet());
-      bullet.circle.strokeWidth = 2;
-      bullet.circle.radius = 4;
-      bullet.circle.fill = am4core.color("#fff");
-      bullet.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+      // var range = valueAxis.axisRanges.create();
+      // range.value = 85;
+      // range.grid.stroke = am4core.color("#396478");
+      // range.grid.strokeWidth = 2;
+      // range.grid.strokeOpacity = 1;
 
       var clickedBullets = []
 
-      bullet.events.on('hit', (ev) => {
-        this.point = ev.target?.dataItem?.dataContext
+      const createSeries = (valueProp) => {
+        // Create series
+        var series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = valueProp;
+        series.dataFields.dateX = "date";
+        // series.tooltipText = "{value}"
+        series.strokeWidth = 2;
+        series.minBulletDistance = 15;
+        series.dataFields.openValueY = "open";
+        series.dataFields.closeValueY = "close";
 
-        chart.series.values.forEach(element => {
-          element.strokeOpacity = 0.5
-        });
-        
-        // reset styles circles
-        clickedBullets.forEach(item => {
-          item.fill = am4core.color("#fff")
-          item.scale = 1
+        // Make bullets grow on hover
+        var bullet = series.bullets.push(new am4charts.CircleBullet());
+        bullet.circle.strokeWidth = 2;
+        bullet.circle.radius = 4;
+        bullet.circle.fill = am4core.color("#fff");
+        bullet.cursorOverStyle = am4core.MouseCursorStyle.pointer;
+
+        bullet.events.on('hit', (ev) => {
+          ev.event.stopPropagation()
+          this.point = this.$moment(ev.target?.dataItem?.dataContext?.date).format('yyyy-MM-DD HH:mm:ss')
+
+          chart.series.values.forEach(element => {
+            element.strokeOpacity = 0.5
+          });
+          
+          // reset styles circles
+          clickedBullets.forEach(item => {
+            item.fill = am4core.color("#fff")
+            item.scale = 1
+          })
+          clickedBullets = []
+          // init circles styles
+          ev.target.circle.fill = am4core.color("#67b7dc");
+          ev.target.circle.scale = 1.3
+          // ev.target.circle.radius = 2;
+
+          clickedBullets.push(ev.target.circle)
         })
-        clickedBullets = []
-        // init circles styles
-        ev.target.circle.fill = am4core.color("#67b7dc");
-        ev.target.circle.scale = 1.3
-        // ev.target.circle.radius = 2;
+      }
 
-        clickedBullets.push(ev.target.circle)
-      })
-
-
-      // bullet.circle.adapter.add("fill", function(text, target) {
-      //   var data = target.dataItem.dataContext;
-      //   return text;
-      // });
+      createSeries('value')
 
       chart.events.on('hit', (ev) => {
-        //нажимаем на буллет, событие всплывает на график
-        if (ev.event?.target?.tagName === 'circle') {
+        if (ev.event.cancelBubble) {
           return
         }
         // reset styles circles
@@ -303,15 +336,17 @@ export default {
       chart.cursor = new am4charts.XYCursor();
       chart.cursor.behavior = "panXY";
       // chart.cursor.xAxis = dateAxis;
-      chart.cursor.snapToSeries = series;
+      // chart.cursor.snapToSeries = series;
       chart.cursor.opacity = 0;
       chart.cursor.lineY.opacity = 0;
       chart.cursor.lineX.opacity = 0;
+      chart.scrollbarX = new am4core.Scrollbar();
+      // chart.scrollbarX.events.on('wheel', (ev) => {
+      //   console.log(ev);
+      // })
+      chart.scrollbarX.disabled = true;
       chart.zoomOutButton.disabled = true;
-      chart.maskBullets = false;
-      
-      dateAxis.keepSelection = true;
-      dateAxis.tooltip.disabled = true;
+      chart.swipeable = true;
     }
   },
   beforeDestroy() {
